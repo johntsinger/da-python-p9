fa3861a544a66ce42453ea2683bce35163bd9e6f
John <john.t.singer@gmail.com>
2023-08-19 21:34:06 +0200

    update ticket_snippet.html and feed.html, update view remake queries, update urls

--- litrevu/reviews/urls.py
+++ litrevu/reviews/urls.py
@@ -3,12 +3,10 @@ from reviews import views as re_views
 
 urlpatterns = [
     path('', re_views.IndexView.as_view(), name='feed'),
-    path('reviews/user-posts/<int:user_id>', re_views.TicketListView.as_view(),
+    path('reviews/user-posts/<int:user_id>', re_views.UserPostView.as_view(),
          name='user-posts'),
     path('reviews/ticket/<int:pk>', re_views.TicketDetailView.as_view(),
          name='ticket-detail'),
-    path('reviews/review/<int:pk>', re_views.ReviewDetailView.as_view(),
-         name='review-detail'),
     path('reviews/ticket/create/', re_views.TicketCreateView.as_view(),
          name='create-ticket'),
     path('reviews/ticket/<int:pk>/change', re_views.TicketUpdateView.as_view(),
@@ -16,5 +14,8 @@ urlpatterns = [
     path('reviews/ticket/<int:pk>/delete', re_views.TicketDeleteView.as_view(),
          name='delete-ticket'),
     path('reviews/review/create', re_views.ReviewCreateView.as_view(),
+         name='create-review'),
+    # to create review for an existing ticket
+    path('reviews/review/create/<int:pk>', re_views.ReviewCreateView.as_view(),
          name='create-review'),
 ]
--- litrevu/reviews/views.py
+++ litrevu/reviews/views.py
@@ -1,13 +1,14 @@
-from itertools import chain
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from django.db.models import Q, Case, When
 from django.views import View
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from django.db.models import Q, CharField, Value
 from reviews.forms import TicketForm
 from reviews.models import Ticket, Review
 
@@ -18,33 +19,59 @@ class TicketBaseView(LoginRequiredMixin, SuccessMessageMixin, View):
     success_url = reverse_lazy('feed')
 
 
-class IndexView(ListView):
+class IndexView(LoginRequiredMixin, ListView):
     template_name = 'reviews/feed.html'
 
     def get_queryset(self):
         tickets = Ticket.objects.filter(
+            # current user tickets
             Q(user=self.request.user)
+            # tickets from users followed by the current user
             | Q(user_id__in=self.request.user.following.values_list(
                 "followed_user"))
-        )
+            # tickets answered by users followed by current user
+            | Q(review__user__id__in=self.request.user.following.values_list(
+                "followed_user"))
+        ).annotate(
+            # annotate date = ticket time_created if no review for this
+            # ticket else review__time_created
+            date=Case(
+                When(review__isnull=False, then="review__time_created"),
+                default="time_created"
+            )
+        ).order_by("-date")  # sort by reverse (-) date from annotate
+
         return tickets
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['tickets'] = self.get_queryset()
+        print('Context IndexView', context)
 
-        print(context)
         return context
 
 
-class TicketListView(TicketBaseView, ListView):
+class UserPostView(TicketBaseView, ListView):
     template_name = 'reviews/feed.html'
 
     def get_queryset(self):
-        if self.kwargs:
-            return Ticket.objects.filter(user_id=self.kwargs['user_id'])
-        else:
-            return Ticket.objects.all()
+        user = User.objects.get(id=self.kwargs['user_id'])
+        tickets = (
+            user.ticket_set.all()
+            | Ticket.objects.filter(review__user=user)
+        ).annotate(
+            date=Case(
+                When(review__isnull=False, then="review__time_created"),
+                default="time_created"
+            )
+        ).order_by("-date")
+
+        return tickets
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print(context)
+
+        return context
 
 
 class TicketDetailView(TicketBaseView, DetailView):
@@ -57,6 +84,7 @@ class TicketCreateView(TicketBaseView, CreateView):
 
     def form_valid(self, form):
         form.instance.user = self.request.user
+
         return super().form_valid(form)
 
 
@@ -72,6 +100,7 @@ class TicketDeleteView(TicketBaseView, DeleteView):
             self.request,
             'Your ticket has been sucessfully deleted !'
         )
+
         return response
 
 
@@ -84,33 +113,44 @@ class ReviewBaseView(LoginRequiredMixin, SuccessMessageMixin, View):
 class ReviewCreateView(ReviewBaseView, CreateView):
     template_name = 'reviews/review_create.html'
     success_url = reverse_lazy('feed')
+    success_message = 'Your review has been successfully created !'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['ticket_form'] = TicketForm()
+        ticket = None
+        if self.kwargs:
+            ticket = get_object_or_404(Ticket, pk=self.kwargs['pk'])
+        context['ticket_form'] = TicketForm(
+            instance=ticket
+        )
 
         return context
 
     def post(self, request, *args, **kwargs):
         form = self.get_form()
         ticket_form = TicketForm(request.POST, request.FILES)
+
         if form.is_valid() and ticket_form.is_valid():
             return self.form_valid(form, ticket_form)
         else:
             return self.form_invalid(form)
 
     def form_valid(self, form, form2):
-        t = form2.save(commit=False)
-        t.user = self.request.user
-        t.save()
+        if not self.kwargs:
+            ticket = form2.save(commit=False)
+            ticket.user = self.request.user
+            ticket.save()
+        else:
+            ticket = get_object_or_404(Ticket, pk=self.kwargs['pk'])
 
         review = form.save(commit=False)
-        review.ticket_id = t.id
+        review.ticket_id = ticket.id
         review.user = self.request.user
         review.save()
 
         return super().form_valid(form)
 
 
-class ReviewDetailView(ReviewBaseView, DetailView):
-    template_name = 'reviews/ticket_detail.html'
+class ReviewUpdateView(ReviewBaseView, UpdateView):
+    template_name = 'reviews/review_change.html'
+    success_message = 'Your review has been successfully updated !'
