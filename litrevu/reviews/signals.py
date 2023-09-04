d6015442aa9ffffbf0b177a7ebb391527117f8d5
John <john.t.singer@gmail.com>
2023-09-04 23:17:32 +0200

    Add signal to delete folder if image is deleted

--- litrevu/reviews/signals.py
+++ litrevu/reviews/signals.py
@@ -15,8 +15,6 @@ def delete_empty_folder(sender, **kwargs):
     is empty if it is delete the folder.
     """
     path = Path(settings.MEDIA_ROOT, kwargs['file_name']).parent
-    print("le path")
-    print(path)
     if path.exists():
         if not next(path.iterdir(), None):
             path.rmdir()
